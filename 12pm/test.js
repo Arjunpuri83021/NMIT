31644b4b0dc1d3f75220f17252253a955c2e99a5
arjun puri <thevirus991@gmail.com>
2024-06-25 11:11:35 +0530

    sdas

---
+++ 12pm/test.js
@@ -0,0 +1,260 @@
+// console.log("hello arjun")
+
+
+// var a = 2
+// var name = "arjun"
+// var name = "manish"  ///  value can chnage every type
+// name = "puri"
+// // console.log(a,name)
+
+// let b =5
+// let city = "jaipur"
+// city = "udaipur"  // value can be change with only varibale name
+// // console.log(b,city)
+
+// const c =10
+// const state ="rajsthan"
+// const state = "MP"
+// state = "MP"     //// value can not change
+// console.log(c,state)
+
+
+
+// var a = 2
+// var b = 5
+// var a = "arjun"
+// var b = "puri"
+
+// var c = a+" "+b
+
+// console.log(c)
+
+
+
+
+// let b = 5
+// let a = 10
+
+// console.log(b*a)
+
+
+// let a = 10
+// let b = 20
+
+// if(a>b){
+//     console.log("hello")
+// }
+
+// else if(b<b){
+//     console.log("hello 2")
+// }
+
+// else if(a!=b){
+//     console.log("hello 3")
+// }
+// else{
+//     console.log("nothing")
+// }
+
+
+
+
+// let age = 36
+
+
+
+// if(age>=18 && age<=24){
+//     console.log("adult")
+// }
+
+// else if(age<=17){
+//     console.log("child")
+// }
+
+// else if(age >= 25 && age <=35){
+//     console.log("man")
+// }
+
+// else if(age >= 35 && age <=59){
+//     console.log("older")
+// }
+
+// else if(age >=60 && age <=85){
+//     console.log("aged")
+// }
+// else{
+//     console.log("death")
+// }
+
+
+      /// 0 start 
+// const array = [3,5,1,44,32]
+// const array2 = ["arjun","puri","mnish","baba","goswami","annu"]
+
+// //  array.push("chodhary") /// add a element in last
+// //  array.unshift("aman") // add a element in start
+// //    array.pop() // remove last element
+// // array.shift() // remove first element
+// // array2.sort() // for sort all array alphabaticlly
+// // const count=array2.length //// for count all elements
+
+// // const newarray = array.slice(4,5)
+// //  array.splice(1,3,12,13,14)
+// console.log(array)
+
+
+
+// let age = 61
+
+// switch(true){
+//       case age >=18 && age <=25:
+//             console.log("your yunger")
+//             break;
+//        case age >=25 && age <=35:
+//             console.log("your man")
+//             break;
+//             case age >=35 && age <=60:
+//             console.log("your older")
+//             break;
+//             default:
+//                   console.log("deathed")
+// }
+
+
+//  function handel(){
+//       let age = 61
+
+// switch(true){
+//       case age >=18 && age <=25:
+//             console.log("your yunger")
+//             break;
+//        case age >=25 && age <=35:
+//             console.log("your man")
+//             break;
+//             case age >=35 && age <=60:
+//             console.log("your older")
+//             break;
+//             default:
+//                   console.log("deathed")
+// }
+//  }
+// handel()
+ 
+
+//  const handel2 = ()=>{
+//       let a =2
+//       console.log(a)
+//  }
+//  handel2()
+ 
+
+
+// function handel(a,b){
+//       console.log(a+b)
+// }
+// handel(2,5)
+// handel(5,8)
+// handel(5,28)
+// handel(5,82)
+
+
+
+
+// const obj = {
+//       name:"arjun",
+//       age:23,
+//       perfosion:"developer"
+// }
+
+// console.log(obj.age,obj.name)
+
+
+// let a = 1
+// while(a<=10){
+//       console.log(a*8)
+//       a++
+// }
+ 
+ 
+// for(let a =1; a<=10;a++){
+//       console.log(a*2)
+// }
+
+
+// "use strict"
+// let a =2
+// console.log(a)
+
+
+// function one(){
+//       two()
+// }
+
+// function two(){
+//       console.log("hello arjun")
+// }
+
+// one()
+// one()
+// one()
+// one()
+
+
+// "use strict"
+
+// a =2;
+// var a;
+
+// console.log(a)
+
+
+// let value = null
+// let newvalue = value ?? 10
+
+// console.log(newvalue)
+
+
+// setTimeout(() => {
+//       console.log("hello mahendra")
+// }, 1000);
+
+
+
+// setInterval(() => {
+//      console.log("hello madhu") 
+// }, 1000);
+
+
+// let array = [2,4,6,8]
+
+// let newArray = array.map((x)=>{
+//     return x*2
+// })
+
+// console.log(newArray)
+
+
+// let array = [20,18,16,30,12]
+
+// const newArray = array.filter((age)=>{
+//       return age >=18
+// })
+
+// console.log(newArray)
+ 
+
+// new Promise((resolve,reject,panding)=>{
+//       setTimeout(() => {
+//             const data = "paise"
+//             resolve(data)
+//       }, 1000);
+// }).then((data)=>{
+//   console.log(data,"de raha hu")
+// }).catch((data)=>{
+//       console.log(data,"nhi de raha")
+// })
+
+
+
+
+
