e2dbc4128735b29de49293e441f954f7a63de6ed
arunbhakar <bhakararun68@gmail.com>
2024-07-28 14:19:36 +0530

    arun

---
+++ new/script.js
@@ -0,0 +1,9 @@
+
+document.getElementById("openNav").onclick = function() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  document.getElementById("closeNav").onclick = function() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  
